d7f346e3aac5924e689638fad10e77b858ef1de1
liuyiqi1999 <liuyiqi1999@163.com>
2019-05-15 21:52:39 +0800

    separator update

--- lab8/functions.inc.php
+++ lab8/functions.inc.php
@@ -5,10 +5,10 @@
         echo "<td><img src='images/books/tinysquare/$file'</td>";
         echo "<td>$title</td>";
         echo "<td>$quantity</td>";
-        $price = number_format($price,2);
+        $price = number_format($price,2,".","");
         echo "<td>\$$price</td>";
         $amount = $quantity * $price;
-        $amount = number_format($amount,2);
+        $amount = number_format($amount,2,".","");
         echo "<td>\$$amount</td>";
         echo "</tr>";
     }
